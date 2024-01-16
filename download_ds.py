7a32effc2e659b6c0e7edb84321be840507e1709
vaishnavirshah <vaishnavirahulshah@gmail.com>
2024-01-17 00:05:18 +0530

    final project files

--- download_ds.py
+++ download_ds.py
@@ -14,6 +14,6 @@ def download_json(url, save_path):
 # Replace 'your_json_url' with the actual URL of the JSON file
 json_url = 'https://ciir.cs.umass.edu/downloads/LaMP/LaMP_2/train/train_questions.json'
 # Replace 'output.json' with the desired local file name to save the JSON
-output_path = '/Users/vaishnavishah/Documents/COMPSCI 646/project/lamp2_trainQs.json'
+output_path = '/Users/vaishnavishah/Documents/COMPSCI 646/project/646_project/lamp2_trainQs.json'
 
 download_json(json_url, output_path)
